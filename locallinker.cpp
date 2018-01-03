2b0b7e5ad23b0763077adb6bbca9d78c9fc5d733
RIscRIpt <richard@riscript.com>
2018-01-03 11:16:16 +0200

    fix commit_memory

--- locallinker.cpp
+++ locallinker.cpp
@@ -23,6 +23,10 @@ uint64_t LocalLinker::reserve_memory(uint64_t address, size_t size) const {
 void LocalLinker::commit_memory(uint64_t address, std::vector<std::byte> const &memory, uint32_t protection) const {
     DWORD old_prot = 0;
     LPVOID ptr = reinterpret_cast<void*>(address);
+    LPVOID new_ptr = VirtualAlloc(ptr, memory.size(), MEM_COMMIT, PAGE_READWRITE);
+    if (ptr != new_ptr) {
+        throw 1;
+    }
     memcpy(ptr, memory.data(), memory.size());
     if (!VirtualProtect(ptr, memory.size(), protection, &old_prot)) {
         throw rrl::win::Win32Exception(GetLastError());
